9cf9993fcf2c741f939ae2a357cf62d022f91569
JensPauwels <jens.pauwels@hotmail.com>
2017-05-01 23:20:46 +0200

    fixed error on string length

--- app.js
+++ app.js
@@ -5,18 +5,24 @@
 const fs = require('fs');
 const readline = require('readline');
 const stream = require('stream');
-let index = 0;
-
-
-//command to run : node --max-old-space-size=8192 app.js
-//add all the tables that you need in the array
-const tablesThatINeed = ['arrival_delay','departure_state','arrival_actual'];
-let tables = [];
-
-
 const instream = fs.createReadStream('./files/Dataset.txt');
 const outstream = new stream;
-var rl = readline.createInterface(instream,outstream);
+const rl = readline.createInterface(instream,outstream);
+const tablesThatINeed = ['date','arrival_delay','departure_state','arrival_actual'];
+let tables = [];
+let index = 0;
+let contentIdx = 1;
+
+let contents = {
+  content1: '',
+  content2: '',
+  content3: '',
+  content4: '',
+  content5: ''
+}
+
+let currentContent = contents[`content${1}`];
+
 
 const determineTables = (row) => {
   row.split(',').forEach((column,index) => {
@@ -24,23 +30,40 @@ const determineTables = (row) => {
   });
 };
 
-let content = '';
+
 
 rl.on('line',(row) => {
   if(index === 0) determineTables(row);
   row.split(',').forEach((column,index) => {
+    if( contents[`content${contentIdx}`].length > 100000000 ) {
+      contentIdx++;
+    }
     if(tables.indexOf(index) !== -1) {
-      content += (index === tables[tables.length - 1]) ? column : `${column},`;
+      if(index === tables[tables.length - 1] ){
+        contents[`content${contentIdx}`] += column;
+      }
+      else{
+        contents[`content${contentIdx}`] += `${column},`;
+      }
     }
   });
-  content += '\n';
+  contents[`content${contentIdx}`] += '\n';
   console.log(index);
   index++;
 });
 
 rl.on('close', () => {
-  fs.writeFile('./files/newData.txt',content,'utf8', (err) => {
+  console.log(contents.content1);
+  fs.writeFile('./files/newData.txt',contents.content1,'utf8', (err) => {
     if (err) console.log(err);
     console.log('thanks for using the service!');
   });
+
+  for(let i = 2 ; i < 6 ; i++){
+    if(contents[`content${i}`] !== ''){
+      fs.appendFile('./files/newData.txt',contents[`content${i}`],'utf8', (err) => {
+        if (err) console.log(err);
+      });
+    }
+  };
 });
